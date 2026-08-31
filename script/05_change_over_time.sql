6d8cc6eb80e78a90bcda1ed5a4f0039b553d5d84
Esma <esmaa.bulut0135@gmail.com>
2026-09-01 02:39:05 +0300

    Add monthly sales and order trend analysis queries
    Added SQL queries to analyze monthly sales and order trends.

--- script/05_change_over_time.sql
+++ script/05_change_over_time.sql
@@ -1,1 +1,23 @@
 
+-- ===================================================
+-- Change Over Time
+-- Amaç: Aylık bazda satış ve sipariş trendini incelemek
+-- ===================================================
+
+-- Aylık toplam satış
+SELECT
+    DATETRUNC(month, o.order_purchase_timestamp) AS month,
+    ROUND(SUM(t.price + t.freight_value), 2) AS TotalSales
+FROM dbo.olist_order_items_dataset t
+LEFT JOIN dbo.olist_orders_dataset o
+    ON t.order_id = o.order_id
+GROUP BY DATETRUNC(month, o.order_purchase_timestamp)
+ORDER BY DATETRUNC(month, o.order_purchase_timestamp);
+
+-- Aylık toplam sipariş sayısı
+SELECT
+    DATETRUNC(month, order_purchase_timestamp) AS month,
+    COUNT(order_id) AS totalOrder
+FROM dbo.olist_orders_dataset
+GROUP BY DATETRUNC(month, order_purchase_timestamp)
+ORDER BY DATETRUNC(month, order_purchase_timestamp);
