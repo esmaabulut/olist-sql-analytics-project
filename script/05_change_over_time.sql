0aab6cbfcded7b08288b8b28e7064dd5c0255e6d
Esma <esmaa.bulut0135@gmail.com>
2026-09-02 01:35:35 +0300

    Translate comments and format SQL queries
    Updated comments to English and improved formatting.

--- script/05_change_over_time.sql
+++ script/05_change_over_time.sql
@@ -1,23 +1,20 @@
-
 -- ===================================================
 -- Change Over Time
--- Amaç: Aylık bazda satış ve sipariş trendini incelemek
+-- Purpose: Analyze monthly sales and order trends
 -- ===================================================
 
--- Aylık toplam satış
-SELECT
-    DATETRUNC(month, o.order_purchase_timestamp) AS month,
-    ROUND(SUM(t.price + t.freight_value), 2) AS TotalSales
+-- Total monthly sales
+SELECT DATETRUNC(month, o.order_purchase_timestamp) AS month, 
+       ROUND(SUM(t.price + t.freight_value), 2) AS TotalSales
 FROM dbo.olist_order_items_dataset t
-LEFT JOIN dbo.olist_orders_dataset o
-    ON t.order_id = o.order_id
+LEFT JOIN dbo.olist_orders_dataset o 
+ON t.order_id = o.order_id
 GROUP BY DATETRUNC(month, o.order_purchase_timestamp)
 ORDER BY DATETRUNC(month, o.order_purchase_timestamp);
 
--- Aylık toplam sipariş sayısı
-SELECT
-    DATETRUNC(month, order_purchase_timestamp) AS month,
-    COUNT(order_id) AS totalOrder
+-- Total monthly orders
+SELECT DATETRUNC(month, order_purchase_timestamp) AS month, 
+       COUNT(order_id) AS totalOrder
 FROM dbo.olist_orders_dataset
 GROUP BY DATETRUNC(month, order_purchase_timestamp)
 ORDER BY DATETRUNC(month, order_purchase_timestamp);
