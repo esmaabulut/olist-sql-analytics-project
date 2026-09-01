78b332c2d1e0139feec62312db95c652ec45e00e
Esma <esmaa.bulut0135@gmail.com>
2026-09-02 01:34:48 +0300

    Translate SQL comments from Turkish to English

--- script/02_measures.sql
+++ script/02_measures.sql
@@ -1,32 +1,32 @@
 -- ===================================================
 -- Measures
--- Amaç: Dimension'a bağlı olmadan genel özet (büyük) sayıları hesaplamak
+-- Purpose: Calculate general summary metrics independently of dimensions
 -- ===================================================
 
--- Toplam sipariş sayısı
-SELECT COUNT(DISTINCT order_id) AS totalOrder 
+-- Total number of orders
+SELECT COUNT(DISTINCT order_id) AS totalOrder
 FROM dbo.olist_orders_dataset;
 
--- Ortalama ürün fiyatı
-SELECT ROUND(AVG(price), 2) AS avgPrice  
+-- Average product price
+SELECT ROUND(AVG(price), 2) AS avgPrice
 FROM dbo.olist_order_items_dataset;
 
--- Ortalama kargo ücreti
-SELECT ROUND(AVG(freight_value), 2) AS avgFreight 
+-- Average freight cost
+SELECT ROUND(AVG(freight_value), 2) AS avgFreight
 FROM dbo.olist_order_items_dataset;
 
--- Toplam farklı ürün sayısı
-SELECT COUNT(DISTINCT product_id) AS totalProduct 
+-- Total number of distinct products
+SELECT COUNT(DISTINCT product_id) AS totalProduct
 FROM dbo.olist_products_dataset;
 
--- Toplam farklı kategori sayısı
-SELECT COUNT(DISTINCT product_category_name) AS totalCategory 
+-- Total number of distinct categories
+SELECT COUNT(DISTINCT product_category_name) AS totalCategory
 FROM dbo.olist_products_dataset;
 
--- Toplam müşteri sayısı
-SELECT COUNT(DISTINCT customer_id) AS totalCustomer 
+-- Total number of customers
+SELECT COUNT(DISTINCT customer_id) AS totalCustomer
 FROM dbo.olist_customers_dataset;
 
--- Toplam farklı şehir sayısı
-SELECT COUNT(DISTINCT customer_city) AS totalCity 
+-- Total number of distinct cities
+SELECT COUNT(DISTINCT customer_city) AS totalCity
 FROM dbo.olist_customers_dataset;
